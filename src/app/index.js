804f161c795987ea956c5dcd02a159a3cc74de12
rifaldhiaw <rifaldhiaw@gmail.com>
2023-12-19 10:37:25 +0700

    downgrade react-native-safe-area-context": "4.6.3"

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const insets = useSafeAreaInsets();
+
+  return (
+    <div>
+      <p>Downgrade react-native-safe-area-context": "4.6.3"</p>
+      <p>insets : {JSON.stringify(insets)}</p>
+    </div>
+  );
 }
